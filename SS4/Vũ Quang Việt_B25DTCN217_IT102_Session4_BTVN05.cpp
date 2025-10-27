512dfe719e8d1bea4c356cfd70e507719d30826a
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-27 17:19:03 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN05.cpp

--- SS4/Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN05.cpp
+++ SS4/Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN05.cpp
@@ -1,30 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 int main() {
-    int chiSoCu, chiSoMoi;
-    int soDien;
-    printf("Nhap chi so cu (kWh): ");
-    scanf("%d", &chiSoCu);
-	printf("Nhap chi so moi (kWh): ");
-    scanf("%d", &chiSoMoi);
-
-    soDien = chiSoMoi - chiSoCu;
-
-    if (soDien < 0) {
-        printf("Chi so moi phai lon hon chi so cu!\n");
-        return 1;
-    }
-
-    if (soDien < 50)
-        printf("0<= So dien <50");
-    else if (soDien < 100)
-        printf("50<= So dien <100");
-    else if (soDien < 150)
-        printf("100<= So dien <150");
-    else if (soDien < 200)
-        printf("150<= So dien <200");
-    else
-        printf("200<= So dien");
-    return 0;
+	int a,b,c;
+	printf("Nhap so thu nhat: ");
+	scanf("%d", &a);
+	printf("Nhap so thu hai: ");
+	scanf("%d", &b);
+	printf("Nhap so thu ba: ");
+	scanf("%d", &c);
+	if(a<c && c<b)
+	   printf("So thu 3 nam trong khoang giua so thu 1 va 2");
+	else if(a>c && c<b)
+	   printf("So thu 3 nam trong khoang giua so thu 1 va 2");
+	else
+	   printf("So thu 3 ko nam trong khoang giua so thu 1 va 2");
+	return 0;
 }
-
