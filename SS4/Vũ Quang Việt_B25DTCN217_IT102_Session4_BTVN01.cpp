875390747084e35a8b95f39d52f9f1116a27e06f
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-27 14:00:25 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN01.cpp

--- SS4/Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN01.cpp
+++ SS4/Vũ Quang Việt_B25DTCN217_IT102_Session4_BTVN01.cpp
@@ -4,9 +4,13 @@ int main(){
 	printf("Nhap so nguyen bat ki: ");
 	scanf("%d", &n);
 	if(n < 0){
-		printf("Day la so am \n");
-	} else {
-		printf("Day la so duong \n");
+		printf("%d la so am \n", n);
+	} else if(n==0){
+	    printf("%d ko phai la so am cung ko la so duong", n);
+	}
+	else{
+		printf("%d la so duong \n", n);
 	}
 	return 0;
 }
+
